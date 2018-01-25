190206b2ec9a5273cbaf9cfb3d1174db5edc4b9d
Brian Bolnick <brianbolnick@gmail.com>
2018-01-25 10:25:20 -0700

    initial commit

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.svg';
-import '../css/App.css';
 
-class App extends Component {
+class NotFoundPage extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">404</h1>
-        </header>
-        <p className="App-intro">
-          Sorry, that page could not be found.
-        </p>
+      <div className="not-found-page">
+        404! 
       </div>
     );
   }
 }
 
-export default App;
+export default NotFoundPage;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import 'semantic-ui-css/semantic.min.css';
 import "./css/index.css";
 import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 import store, { history } from './tools/store';
@@ -17,5 +17,3 @@ ReactDOM.render((
       </ConnectedRouter>
   </Provider>
 ), document.getElementById('root'))
-registerServiceWorker();
-
