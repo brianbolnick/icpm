37db4d5c20f46ddd2e3c41b56c6ff1d93c178d96
Brian Bolnick <brianbolnick@gmail.com>
2018-01-24 23:32:47 -0700

    add new client test

--- client/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
-// import ProfileReducer from './profile_reducer';
-import { reducer as formReducer } from 'redux-form';
-
-export default combineReducers({
-    routing: routerReducer,
-    form: formReducer
-});
