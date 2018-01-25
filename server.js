04c20138ac52c2cf94c5ac9f22fb82e64b79e756
Brian Bolnick <brianbolnick@gmail.com>
2018-01-24 22:29:14 -0700

    update server js file

--- server.js
+++ server.js
@@ -17,29 +17,39 @@ var port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-    //and remove cacheing so we get the most recent comments
-    res.setHeader('Cache-Control', 'no-cache');
-    next();
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+//     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//     //and remove cacheing so we get the most recent comments
+//     res.setHeader('Cache-Control', 'no-cache');
+//     next();
+// });
+
+app.get('/api/test', (req, res) => {
+    // Return them as json
+    res.json({ test: "testing!" });
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
-
-//Endpoinys 
-router.get('/', function (req, res) {
-    res.json({ message: 'API Initialized!' });
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 
-app.use('/api/v1', router);
+// //Endpoints 
+// router.get('/', function (req, res) {
+//     res.json({ message: 'API Initialized!' });
+// });
+
+// router.get('/testing', function (req, res) {
+//     res.json({ message: 'Test!' });
+// });
+
+
+// app.use('/api/v1', router);
 
 app.listen(port, function () {
     console.log(`api running on port ${port}`);
