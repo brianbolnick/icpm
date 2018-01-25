82716c04c02a597ebb22762569d264d388bff068
Brian Bolnick <brianbolnick@gmail.com>
2018-01-25 07:47:59 -0700

    add router and redux configs

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react';
-import logo from '../img/logo.svg';
 import '../css/App.css';
+import HomePage from './HomePage.js'
+import NotFound from './NotFound.js'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Route, withRouter, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
+    const currentKey = this.props.location.pathname.split("/")[1] || "/";
+    const timeout = { enter: 300, exit: 200 };
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <TransitionGroup component="main" className="page-main" style={{ height: '100%' }}>
+        <CSSTransition
+          key={currentKey}
+          timeout={timeout}
+          classNames="fade"
+          appear
+        >
+          <Switch location={this.props.location}>
+            <Route path="/" exact component={HomePage} />
+            <Route component={NotFound} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './css/index.css';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./css/index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './tools/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+  <Provider store={store}>
+      <ConnectedRouter history={history}>
+          <div id='app'>
+              <App />
+          </div>
+      </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'))
+registerServiceWorker();
+
