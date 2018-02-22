de573177b751bd58af0374549e0bab1b27c12555
Brian Bolnick <brianbolnick@gmail.com>
2018-02-22 11:48:59 -0700

    add action item handling

--- client/src/components/Projects/NotesView.js
+++ client/src/components/Projects/NotesView.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import ReactQuill from 'react-quill';
 import { Icon } from 'semantic-ui-react'
+import IconInstructureLine from 'instructure-icons/lib/Line/IconInstructureLine'
 import 'react-quill/dist/quill.snow.css';
 
 class NotesContent extends Component {
@@ -13,7 +14,22 @@ class NotesContent extends Component {
             migrationHtml: '',
             supportHtml: '',
             trainingHtml: '',
-            otherHtml: ''
+            otherHtml: '',
+            newItem: '',
+            newItemTeam: 2,
+            newItemActive: false,
+            actionItems: [
+                {
+                    id: 1,
+                    description: "Compile project plan",
+                    team: 1,
+                },
+                {
+                    id: 2,
+                    description: "Send list of training users",
+                    team: 2,
+                }
+            ]
         }
         this.handleSisChange = this.handleSisChange.bind(this)
         this.handleAuthChange = this.handleAuthChange.bind(this)
@@ -23,6 +39,11 @@ class NotesContent extends Component {
         this.handleTrainingChange = this.handleTrainingChange.bind(this)
         this.handleOtherChange = this.handleOtherChange.bind(this)
         this.handleButtonClick = this.handleButtonClick.bind(this)
+        this.handleAddItemClick = this.handleAddItemClick.bind(this)
+        this.handleNewItemChange = this.handleNewItemChange.bind(this)
+        this.handleNewItemSubmit = this.handleNewItemSubmit.bind(this)
+        this.handleNewItemTeamChange = this.handleNewItemTeamChange.bind(this)
+        this.handleDeleteClick = this.handleDeleteClick.bind(this)
     }
 
     handleSisChange(html) {
@@ -51,6 +72,41 @@ class NotesContent extends Component {
         alert("This won't really do much right now, but check your console for all of the html!")
         console.table(this.state)
     }
+
+    handleAddItemClick() {
+        this.setState({ newItemActive: true })
+    }
+
+    handleNewItemChange(event) {
+        this.setState({ newItem: event.target.value })
+
+    }
+    handleNewItemTeamChange(event) {
+        this.setState({ newItemTeam: event.target.value })
+    }
+
+    handleNewItemSubmit(event) {
+        this.setState({
+            newItemActive: false,
+            actionItems: [
+                ...this.state.actionItems,
+                {
+                    id: this.state.actionItems.length + 1,
+                    description: this.state.newItem,
+                    team: parseInt(this.state.newItemTeam, 10)
+                }
+            ],
+            newItem: ''            
+        })
+
+        event.preventDefault();
+    }
+
+    handleDeleteClick = (id) => e => {
+        this.setState({
+            actionItems: this.state.actionItems.filter((x) => x.id !== id)
+        })
+    }
     render() {
         const { data, active } = this.props;
         const modules = {
@@ -67,13 +123,45 @@ class NotesContent extends Component {
             'link'
         ]
 
+        const actions = this.state.actionItems.map((item, index) => {
+            const icon = item.team === 1 ? <IconInstructureLine className="inst-icon" /> : <Icon name='student' disabled />
+            return (
+                <div className="action-item" key={index}>
+                    <div className="action-item-desc">
+                        {icon} {item.description}
+                    </div>
+                    <div className="action-item-settings">
+                        <div className="action-item-icon"><Icon name='edit' link /></div>
+                        <div className="action-item-icon"><Icon id='remove-icon' name='remove' link onClick={this.handleDeleteClick(item.id)}/></div>
+                    </div>
+                </div>
+            )
+        })
+
         return (
             <div className={active === "notes" ? "task-container" : "project-hidden"}>
                 <div className="notes-content">
                     <div className="notes-secondary">
                         <div className="action-items">
                             <div className="notes-box-title">ACTION ITEMS</div>
-                            <div className="new-action-item">
+                            <div className="action-items-container">
+                                {actions}
+                            </div>
+                            {this.state.newItemActive ?
+                                <div className="action-item-form-container">
+                                    <form className="action-item-form" onSubmit={this.handleNewItemSubmit}>
+                                        <input type="text" value={this.state.newItem} onChange={this.handleNewItemChange} className="action-input"/>
+                                        <select value={this.state.newItemTeam} onChange={this.handleNewItemTeamChange} className="action-select">
+                                            <option value={2}>Client</option>
+                                            <option value={1}>INST</option>
+                                        </select>
+                                        <input type="submit" value="Submit" style={{ display: 'none' }} />
+                                    </form>
+                                </div>
+                                :
+                                null
+                            }
+                            <div className="new-action-item" id='new-action-item' onClick={this.handleAddItemClick}>
                                 <Icon name='plus' /> Add Action Item
                             </div>
                         </div>
